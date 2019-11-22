5896a119230e702fefbf7ba2b4adbfac92b86e6b
riliumph <riliumph@gmail.com>
2019-11-22 11:15:21 +0900

    [add] function that defines binding maps

---
+++ functions/fish_user_key_bindings.fish
@@ -0,0 +1,5 @@
+# The following settings are required for the *marked part
+# item2: Preference -> Profiles -> Keys -> Left Option Key: ESC+ (Not Meta)
+function fish_user_key_bindings
+  bind \e\x7F 'kill-whole-line' # *
+end
