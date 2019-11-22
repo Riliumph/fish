e72f7a70ef57f0da453dea05ca5c4b131f7ad525
riliumph <riliumph@gmail.com>
2019-11-22 12:09:11 +0900

    [add] bind a function that select destination
    bind a function that select destination from current directory as
    shortcut

--- functions/fish_user_key_bindings.fish
+++ functions/fish_user_key_bindings.fish
@@ -4,4 +4,6 @@ function fish_user_key_bindings
   bind \e\x7F 'kill-whole-line' # *
   bind \cR 'peco_history'
   bind \e\e\[A 'cd ../; commandline -f repaint'
+  bind \e\e\[B 'cd (find ./ -maxdepth 1 -mindepth 1 -type d | sort -f | peco); and commandline -f repaint'
 end
+q
