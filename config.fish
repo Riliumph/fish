06f3e64316bd2d229d2d95ed65627fc289568d5b
riliumph <riliumph@gmail.com>
2020-05-17 23:44:05 +0900

    [Update] app config

--- config.fish
+++ config.fish
@@ -6,14 +6,15 @@ set --export FISH_ROOT $HOME/.config/fish
 source $FISH_ROOT/init.fish
 
 # Vim
-#if test type vim ^/dev/null
+#if test (which vim) ^/dev/null
 #  set --export VIM_ROOT $HOME/.config/vim
 #  source $VIM_ROOT/init.fish
 #end
 
 # Pyenv
-#if test which pyenv ^/dev/null
-#	set --export PYENV_ROOT $HOME/.config/pyenv
-#	set PATH $PATH $PYENV_ROOT/bin
-#	source (pyenv init - | psub)
+#if test (which pyenv) ^/dev/null
+#  set --export PYENV_ROOT (which pyenv)
+#  set PATH $PATH $PYENV_ROOT/bin
+#  source (pyenv init - | psub)
 #end
+
