b521b4334a88b7b71246a2ede663bbc8434b0f03
riliumph <riliumph@gmail.com>
2021-07-22 06:50:07 +0900

    [Update] separate vim config from config fish

---
+++ conf.d/vim.fish
@@ -0,0 +1,5 @@
+# Vim
+if [ (which vim) ]
+    set --export VIM_ROOT /Users/riliumph/.config/vim
+    source $VIM_ROOT/init.fish
+end
--- config.fish
+++ config.fish
@@ -4,13 +4,6 @@ set --export OS (uname)
 # Fish
 set --export FISH_ROOT $HOME/.config/fish
 source $FISH_ROOT/init.fish
-
-# Vim
-#if test (which vim) ^/dev/null
-#  set --export VIM_ROOT $HOME/.config/vim
-#  source $VIM_ROOT/init.fish
-#end
-
 # Pyenv
 if test (which pyenv) ^/dev/null
   set --export PYENV_ROOT $HOME/.config/pyenv
