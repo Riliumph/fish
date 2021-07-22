d1e4fdb8011e29c5ea9d26f1099c03ac3fd8033f
riliumph <riliumph@gmail.com>
2021-07-22 14:21:19 +0900

    [Update] env files

--- conf.d/pyenv.fish
+++ conf.d/pyenv.fish
@@ -1,6 +1,13 @@
 # Pyenv
 if [ (which pyenv) ]
-    set -Ux PYENV_ROOT $HOME/.config/pyenv
-    set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
-    status is-login; and pyenv init --path | source
+    set -l PYENV_ROOT_CANDIDATE $HOME/.config/pyenv
+    if [ -d $PYENV_ROOT_CANDIDATE ]
+        set -Ux PYENV_ROOT $PYENV_ROOT_CANDIDATE
+        set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
+        status is-login; and pyenv init --path | source
+    else
+        set_color $fish_color_error
+        echo "Cannot find " $PYENV_ROOT_CANDIDATE
+        set_color $fish_color_normal
+    end
 end
--- conf.d/vim.fish
+++ conf.d/vim.fish
@@ -1,5 +1,12 @@
 # Vim
 if [ (which vim) ]
-    set --export VIM_ROOT /Users/riliumph/.config/vim
-    source $VIM_ROOT/init.fish
+    set -l VIM_ROOT_CANDIDATE /Users/riliumph/.config/vim
+    if [ -d $VIM_ROOT_CANDIDATE ]
+        set --export VIM_ROOT $VIM_ROOT_CANDIDATE
+        source $VIM_ROOT/init.fish
+    else
+        set_color $fish_color_error
+        echo "[ERROR] Cannot find" $VIM_ROOT_CANDIDATE
+        set_color $fish_color_normal
+    end
 end
